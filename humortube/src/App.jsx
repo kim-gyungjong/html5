5637a69fc68009b28f130620b9e798078e3803b5
kim-gyungjong <luck0519@naver.com>
2025-06-26 11:15:32 +0900

    유머튜브 웹서버에 올림

---
+++ humortube/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+
+  const apple = 1
+
+  return ( <div>
+    <h1>유머튜브</h1>
+       <h2>음악</h2>
+       <br/>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/VcXLR3rcdyU?si=BlniwzZN7k7xF_H5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<br/>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/KacvUSBvGhk?si=BqebBu5q_J6Q9V3Z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+<br/>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/IYd1-cPwQCk?si=T6USbtM5CcqY5GcI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+  </div>
+  )
+}
+
+export default App
---
+++ test.js
@@ -0,0 +1,6 @@
+
+const a = 11
+const b = 22
+const c = a + b
+
+console.log(c)
