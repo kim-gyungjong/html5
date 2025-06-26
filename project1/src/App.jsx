5828e62115ff67e8e5c4c042fe96f379b5112337
kim-gyungjong <luck0519@naver.com>
2025-06-26 15:37:20 +0900

    asd

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,22 +1,26 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
   const [lat, set_lat] = useState()
+  const [lon, set_lon] = useState()
+  const [accuracy, set_accuracy] = useState()
 
    navigator.geolocation.getCurrentPosition( (position) => {
         const lat0 = position.coords.latitude;
         const lon0 = position.coords.longitude;
         const accuracy0 = position.coords.accuracy;
                set_lat(lat0)
+               set_lon(lon0)
+               set_accuracy(accuracy0)
    })
 
 
 
   return (
     <div>
-      위도: {lat} <br/>
-      경도: {lon} <br/>
-      정확도 : {accuracy}
+     <h1>위도: {lat}</h1> <br/>
+      <h1>경도: {lon}</h1> <br/>
+      <h1>정확도 :{accuracy}</h1>
     </div>
 
  )
